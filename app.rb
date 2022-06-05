bf4a15ae49994697b1a0a643d71962d128b0e45e
Amir Khan <ak2022dev@gmail.com>
2022-06-05 19:43:42 +0100

    Added dummy output page and read parameters into variables

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
+# in app.rb
+
 require 'sinatra'
+
 get '/' do
   erb(:index)
+end
+
+post '/output' do
+  @name = params[:user_name]
+  puts "@name is #{@name}"
+  @bday_day_of_month = params[:day_of_month]
+  puts "@bday_day_of_month is #{@bday_day_of_month}"
+  @bday_month = params[:month]
+  puts "@bday_month is #{@bday_month}"
+  erb(:output)
 end
 
